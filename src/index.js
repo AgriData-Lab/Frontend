f9dc8a98f590c32a52d426a70c2fbc2ac9f66666
kcw9609 <kang960953@gmail.com>
2025-06-28 05:04:26 +0900

    🔧 Config: base url 수정
    Config: base url 수정- 환경변수로 변경

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import axios from 'axios';
-axios.defaults.baseURL = 'http://localhost:8080';
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
