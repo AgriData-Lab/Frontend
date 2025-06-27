4ed5bc17f969ed956b71af0cabfbf5b042749acc
kcw9609 <kang960953@gmail.com>
2025-06-27 13:41:12 +0900

    🔥 Remove: 사용하지 않는 컴포넌트 삭제
    DistributionMapPage삭제, App.js 설정

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/login/LoginPage.js';
 import SignupPage from './pages/login/SignupPage.js';
 import MapPage from './pages/map/MapPage.tsx';
-import DistributionMapPage from './pages/map/DistributionMapPage.tsx';
 import PriceMapPage from './pages/map/PriceMapPage.tsx';
 import WholesalePricePage from './pages/wholesale_retail/Wholesale-price.tsx';
 import RetailPricePage from './pages/wholesale_retail/Retail-price.tsx';
@@ -16,7 +15,6 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/map" element={<MapPage />} />
-        <Route path="/map/distribution" element={<DistributionMapPage />} />
         <Route path="/map/price" element={<PriceMapPage />} />
         <Route path="/wholesale-price" element={<WholesalePricePage />} />
         <Route path="/retail-price" element={<RetailPricePage />} />
